b38b0ec85aa35a245636be7ee4aea9c5c54d4e7c
Ahmed <muhammadahmed.afzal@ceative.co.uk>
2023-03-24 11:04:20 +0500

    Store setup

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,7 +1,10 @@
+import { useAppSelector } from "@/store/store";
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const LandingPage = () => {
+  const { value } = useAppSelector((state) => state.demo);
+  console.log(value);
+  return <div>LandingPage</div>;
 };
 
-export default Home;
+export default LandingPage;
