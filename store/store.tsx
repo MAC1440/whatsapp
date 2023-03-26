dea3a3d22ef78303017a089f711063122343f25c
Ahmed <muhammadahmed.afzal@ceative.co.uk>
2023-03-26 12:06:39 +0500

    comment deleted

--- store/store.tsx
+++ store/store.tsx
@@ -1,24 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { decremented, incremented } from "./demo/demo-slice";
 import demoReducer from "./demo/demo-slice";
 
 const store = configureStore({
   reducer: { demo: demoReducer },
 });
 
-// Can still subscribe to the store
-// store.subscribe(() => console.log(store.getState()));
-
-// // Still pass action objects to `dispatch`, but they're created for us
-// store.dispatch(incremented());
-// // {value: 1}
-// store.dispatch(incremented());
-// // {value: 2}
-// store.dispatch(decremented());
-// // {value: 1}
 export type AppDispatch = typeof store.dispatch;
-// Export a hook that can be reused to resolve types
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export type RootState = ReturnType<typeof store.getState>;
